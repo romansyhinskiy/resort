7d6707ea82f41b0a6c4b7c9a28cc996e6ee05cf8
Roman Syhinskiy <romansyhinskiy@gmail.com>
2020-10-14 15:26:28 +0300

    first commit

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Name extends Component{
+    render(){
+        return (
+            <div>
+                hello from Sigle room comp
+            </div>
+        )
+    }
+}
